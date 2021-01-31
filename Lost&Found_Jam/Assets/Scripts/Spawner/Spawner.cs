930c4868d1e7e75b99bd4ae1139bf6848994e9bf
Adrien-M-design <61044812+Adrien-M-design@users.noreply.github.com>
2021-01-31 11:44:00 +0100

    Ta daronne elle boit du sprite sa mère

--- Lost&Found_Jam/Assets/Scripts/Spawner/Spawner.cs
+++ Lost&Found_Jam/Assets/Scripts/Spawner/Spawner.cs
@@ -9,6 +9,10 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float _spawnRateMax = 1f;
     [SerializeField] private Transform _objectContainer = null;
     [SerializeField] private ObjectController _objectController = null;
+    [SerializeField] private Sprite[] _objectsRed = null;
+    [SerializeField] private Sprite[] _objectsYellow = null;
+    [SerializeField] private Sprite[] _objectsGreen = null;
+    [SerializeField] private Sprite[] _objectsBlue = null;
 
     private float _spawnRate = 0f;
     private int _objectType = 0;
@@ -38,9 +42,34 @@ public class Spawner : MonoBehaviour
 
     private void Spawn()
     {
+        int rand = Random.Range(0, 14);
+
         ObjectMovement ObjectClone = Instantiate(_gameObjectsToSpawn[_objectType], transform.position, transform.rotation, _objectContainer);
         _objectController.AddItem(ObjectClone.GetComponent<ItemProperties>());
         ObjectClone.Init(_objectController);
+
+        string color = ObjectClone.GetComponent<ItemProperties>().GetObjColor();
+
+        if (color == "RED")
+        {
+            ObjectClone.GetComponent<SpriteRenderer>().sprite = _objectsRed[rand];
+        }
+
+        if (color == "BLUE")
+        {
+            ObjectClone.GetComponent<SpriteRenderer>().sprite = _objectsBlue[rand];
+        }
+
+        if (color == "GREEN")
+        {
+            ObjectClone.GetComponent<SpriteRenderer>().sprite = _objectsGreen[rand];
+        }
+
+        if (color == "YELLOW")
+        {
+            ObjectClone.GetComponent<SpriteRenderer>().sprite = _objectsYellow[rand];
+        }
+
         _timeStamp = 0;
         _spawnRate = Random.Range(_spawnRateMin, _spawnRateMax);
         _objectType = Random.Range(0, 4);
